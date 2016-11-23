5cbddeb5656ca9678270d8e093690eea294e1e23
Elliott Choi <elliottchoi98@gmail.com>
2016-11-23 13:11:27 -0800

    Update DoublyCircularLinkedList.h
    Added functions to complete CDLL

--- DoublyCircularLinkedList.h
+++ DoublyCircularLinkedList.h
@@ -42,8 +42,33 @@ class CircularDoublyLinkedList
 		Node*getHead()const { return m_head; }
 		void move_head(bool directionShift);
 
+
+		//Unused Functions but you apprently have to make them to get 100
+		unsigned int size() const;  // return number of items in the list
+		bool isEmpty() const; // true if list is empty, false otherwise
+		void addHead(T item);  // adds item to front of list
+		T removeHead();  // remove front node, and return the data T found in it;
+						 // PRECONDITION: list must not be empty already
+		T retrieveHead() const; // returns the data T in front node; 
+								// PRECONDITION: list must not be empty already
+		void addTail(T item); // add item to end of list
+		T removeTail(); // remove last node, and return the data T found in it;
+						// PRECONDITION: list must not be empty already
+		T retrieveTail() const; // return data T in last node; 
+								// PRECONDITION: list must not be empty already
+		void add(unsigned int n, T item); // Add a new node containing an item in position n on the list
+										  // PRECONDITION: n must be a valid list position (1<=n<=(size+1))
+		T replace(unsigned int n, T item); // Replace the data stored in position n of the list with
+										   // item, and return the replaced item; 
+										   // PRECONDITION: n must be a valid list position (1<=n<=size)  
+		T remove(unsigned int n); // Remove the node at position n from the list, and return the
+								  // item it contained; PRECONDITION: n must be a valid list position (1<=n<=size)
+		T retrieve(unsigned int n) const; // Return the item value from the node in position n; 
+										  // PRECONDITION: n must be a valid list position (1<=n<=size)
+
 		//Iterator Class
 public:
+
 	class Iterator {// iterators are used for efficient traversal of linked lists
 	private:
 		Node* m_ptr;    // an iterator hides a pointer to node
@@ -117,3 +142,92 @@ void CircularDoublyLinkedList<T>::move_head(bool shiftDirection)
 	}
 }
 
+//Unused Functions
+
+//Return Size
+template <class T>
+unsigned int CircularDoublyLinkedList<T>::size()const
+{
+	return m_size;
+}
+
+//Return if list is empty 
+template <class T>
+bool CircularDoublyLinkedList<T>::isEmpty()const
+{
+	if (m_size == 0)
+	{
+		return 0;
+	}
+	return 1; 
+}
+
+//Add head using add item since add item adds to the head 
+template <class T>
+void CircularDoublyLinkedList<T>::addHead(T item)
+{
+	addItem(T item);
+}
+
+//Remove the head item 
+template <class T>
+T CircularDoublyLinkedList<T>::removeHead()
+{
+	// PRECONDITION: list is not already empty
+	assert((!isEmpty() && "LinkedList is Empty (in removeHead)"));
+	Node * oldNode = m_head;
+	T returnVal = m_head->Value;
+	m_head = m_head->next;
+	m_tail->next =m_head;
+	m_head->previous = m_tail;
+	if (m_head == nullptr) m_tail = nullptr;
+	m_size--;
+	delete oldNode;
+	return returnVal;
+}
+
+template <class T>
+T CircularDoublyLinkedList<T>::retrieveHead()
+{
+	// PRECONDITION: list is not already empty
+	assert((!isEmpty() && "LinkedList is Empty (in retrieveHead)"));
+	return m_head->Value;
+}
+
+template <class T>
+void CircularDoublyLinkedList<T>::addTail(T val)
+{
+	if (isEmpty()) addHead(val);
+	else
+	{
+		Node *temp = new Node(val);
+		temp->previous = m_tail;
+		temp->next = m_head;
+		m_tail = temp;
+		m_size++;
+	}
+}
+
+template <class T>
+void CircularDoublyLinkedList<T>::add(unsigned int n, T val)
+{
+
+}
+
+template <class T>
+T CircularDoublyLinkedList<T>::replace(unsigned int n, T val)
+{
+
+}
+
+template <class T>
+T CircularDoublyLinkedList<T>::remove(unsigned int n)
+{
+
+}
+
+template <class T>
+T CircularDoublyLinkedList::retrieve(unsigned int n) const
+{
+
+}
