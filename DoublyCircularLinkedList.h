c1e3111c5183f8b75d9d996edde0924b16cfdeab
Elliott Choi <elliottchoi98@gmail.com>
2016-11-23 17:41:26 -0800

    Update DoublyCircularLinkedList.h

--- DoublyCircularLinkedList.h
+++ DoublyCircularLinkedList.h
@@ -65,7 +65,7 @@ class CircularDoublyLinkedList
 								  // item it contained; PRECONDITION: n must be a valid list position (1<=n<=size)
 		T retrieve(unsigned int n) const; // Return the item value from the node in position n; 
 										  // PRECONDITION: n must be a valid list position (1<=n<=size)
-
+		void swap(unsigned int x, unsigned int y); 
 		//Iterator Class
 public:
 
@@ -186,14 +186,16 @@ T CircularDoublyLinkedList<T>::removeHead()
 	return returnVal;
 }
 
+//Get HEad
 template <class T>
-T CircularDoublyLinkedList<T>::retrieveHead()
+T CircularDoublyLinkedList<T>::retrieveHead()const
 {
 	// PRECONDITION: list is not already empty
 	assert((!isEmpty() && "LinkedList is Empty (in retrieveHead)"));
 	return m_head->Value;
 }
 
+//Add Tail
 template <class T>
 void CircularDoublyLinkedList<T>::addTail(T val)
 {
@@ -208,26 +210,114 @@ void CircularDoublyLinkedList<T>::addTail(T val)
 	}
 }
 
+//Get Tail Value
+template <class T>
+T CircularDoublyLinkedList<T>::retrieveTail() const
+{
+	return (m_tail->Value);
+}
+
+template <class T>
+T CircularDoublyLinkedList<T>::removeTail()
+{
+	Node * old = m_tail;
+	T returnVal = m_tail->Value;
+	m_tail = m_tail->previous;
+	m_tail->next = m_head;
+	m_head->previous = m_tail;
+
+	if (m_head == nullptr) m_tail = nullptr;
+	m_size--;
+	delete old; // delete that node
+	return returnVal;//return value of the old node had
+}
+
+//Add value at specific point
 template <class T>
 void CircularDoublyLinkedList<T>::add(unsigned int n, T val)
 {
+	// PRECONDITION: n should be a valid position in the list
+	assert(((n >= 1) && (n <= size() + 1) && "LinkedList index is out of bounds (in add)"));
+
+	//Case 
+	if (n == 1)         
+	addHead(val);
+	//Adding within the list 
+	else
+	{
+		Node * here = m_head;
+		for (unsigned int k = 1; k < n - 1; k++)
+			here = here->next;
+		here->next = new Node(item, here->next,here->previous);
+		m_size++;
+	}
+
 
 }
 
 template <class T>
 T CircularDoublyLinkedList<T>::replace(unsigned int n, T val)
 {
+	// PRECONDITION: n must be a valid current list position
+	assert(((n >= 1) && (n <= size()) && "LinkedList index is out of bounds (in replace)"));
+	T returnValue;
 
+	Node * here = m_head;
+	for (unsigned int k = 1; k < n; k++)
+		here = here->next;
+	returnValue = here->value;
+	here->value = val;
+	return returnValue;
 }
 
 template <class T>
 T CircularDoublyLinkedList<T>::remove(unsigned int n)
 {
+	// PRECONDITION: n must be a valid current list position
+	assert(((n >= 1) && (n <= size()) && "LinkedList index is out of bounds (in remove)"));
+	if (n == 1) return removeHead();
+	if (n == size()) return removeTail();
 
+	//Finding index of node 
+	Node * here = m_head;
+	for (unsigned int k = 1; k < n - 1; k++)
+		here = here->next;
+	
+	//Value to be deleted
+	Node * kill = here->next;
+	T returnVal = kill->value;
+	here->next = kill->next;
+	(kill->next)->previous = here;
+	//delete wanted node
+	delete kill;
+	m_size--;
+	return returnVal;
 }
 
 template <class T>
-T CircularDoublyLinkedList::retrieve(unsigned int n) const
+T CircularDoublyLinkedList<T> ::retrieve(unsigned int n) const
 {
+	// PRECONDITION: n must be a valid current list position
+	assert(((n >= 1) && (n <= size()) && "LinkedList index is out of bounds (in retrieve)"));
+	if (n == 1) return retrieveHead();
+	if (n == size()) return retrieveTail();
+	Node * here = m_head;
+	for (unsigned int k = 1; k < n; k++) here = here->next;
+	return here->value;
+}
 
+
+template <class T>
+void CircularDoublyLinkedList<T>::swap(unsigned int x, unsigned int y)
+{
+	Node *begin = m_head;
+	Node *end = m_head;
+	T temp_val;
+	for (int i = 1; i < pos1; i++)
+		begin = begin->next;
+	for (int i = 1; i < pos2; i++)
+		end = end->next;
+	temp_val = begin->Value;
+	begin->Value = end->Value;
+	end->Value = temp_val;
 }
